f1c8bab4ede0d5503931ac9c0105169133bdbdbd
David Adams <adams.david.10@gmail.com>
2018-07-29 11:23:06 -0500

    feat(assets): make assets configurable

--- release.config.js
+++ release.config.js
@@ -1,3 +1,15 @@
+let assets = [
+  'package.json',
+  'yarn.lock',
+  'CHANGELOG.md'
+]
+
+const assetsEnv = String(process.env.ASSETS || '').trim()
+if (assetsEnv.length > 0) {
+  const additionalAssets = assetsEnv.split(',')
+  assets = assets.concat(additionalAssets)
+}
+
 module.exports = {
   npmPublish: false,
   verifyConditions: ['@semantic-release/changelog', '@semantic-release/github'],
@@ -6,13 +18,7 @@ module.exports = {
     '@semantic-release/npm',
     {
       path: '@semantic-release/git',
-      assets: [
-        'package.json',
-        'yarn.lock',
-        'CHANGELOG.md',
-        'docs',
-        'dist'
-      ]
+      assets: assets
     }
   ],
   publish: ['@semantic-release/github']
